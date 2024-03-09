2c81840d408fbf532f42c9496b4d1d9ed5aa9fc5
Nicolas HENDRIKX <n.hendrikx@helmo.be>
2024-03-09 10:10:49 +0100

    Add ReverseProxyLinksMiddleware.cs

--- Puroguramu.App/Program.cs
+++ Puroguramu.App/Program.cs
@@ -1,9 +1,11 @@
+using Puroguramu.App.Middlewares;
 using Puroguramu.Domains;
 using Puroguramu.Infrastructures.Dummies;
 using Puroguramu.Infrastructures.Roslyn;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ILogger, Logger<object>>();
+builder.Services.AddScoped<ReverseProxyLinksMiddleware>();
 builder.Services.AddScoped<IExercisesRepository, DummyExercisesRepository>();
 builder.Services.AddScoped<IAssessExercise, RoslynAssessor>();
 
@@ -16,6 +18,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions());
+app.UseReverseProxyLinks();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
