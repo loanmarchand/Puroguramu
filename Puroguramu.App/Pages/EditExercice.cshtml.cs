de1922c8febb5e0369535131bf81fdb95e0f8fec
Q210027 <r.coibion@student.helmo.be>
2024-04-09 15:58:30 +0200

    Pages d'édition de lecon créée. A ajouter dans la liste des lecons pour les profs.
    Pages d'édition d'exercice créée.
    A ajouter dans la liste des exercices pour les profs. Changer le type d'input pour le modele et la solution ? (Comme dans exercices)

---
+++ Puroguramu.App/Pages/EditExercice.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Puroguramu.App.Pages;
+
+public class EditExercice : PageModel
+{
+
+    public string ReturnUrl { get; set; }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public class InputModel
+    {
+        public string Titre { get; set; }
+
+        public string Enonce { get; set; }
+
+        public string Modele { get; set; }
+
+        public string Solution { get; set; }
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
