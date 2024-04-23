587f85a025ac2c6b7b4eccf5575f7ecc0ee8d2bb
loan <l.marchand@student.helmo.be>
2024-04-23 18:01:20 +0200

    Update UserConfiguration

--- Puroguramu.Infrastructures/Configuration/UtilisateursConfiguration.cs
+++ Puroguramu.Infrastructures/Configuration/UtilisateursConfiguration.cs
@@ -7,31 +7,5 @@ namespace Puroguramu.Infrastructures.Configuration;
 
 public class UtilisateursConfiguration : IEntityTypeConfiguration<Utilisateurs>
 {
-    public void Configure(EntityTypeBuilder<Utilisateurs> builder)
-    {
-        builder.HasIndex(utilisateurs => utilisateurs.Matricule).IsUnique();
-        SeedData(builder);
-    }
-
-    private static void SeedData(EntityTypeBuilder<Utilisateurs> builder)
-    {
-        var hasher = new PasswordHasher<Utilisateurs>();
-        var adminPassword = hasher.HashPassword(null!, "Romain1*");
-        var admin = new Utilisateurs
-        {
-            Id = Guid.NewGuid().ToString(),
-            Matricule = "P200000",
-            Nom = "admin",
-            Prenom = "admin",
-            Email = "admin@example.com",
-            Groupe = "0",
-            NormalizedEmail = "ADMIN@EXAMPLE.COM",
-            UserName = "admin@example.com",
-            NormalizedUserName = "ADMIN@EXAMPLE.COM",
-            PasswordHash = adminPassword,
-            SecurityStamp = Guid.NewGuid().ToString(),
-            Role = Role.Teacher,
-        };
-        builder.HasData(admin);
-    }
+    public void Configure(EntityTypeBuilder<Utilisateurs> builder) => builder.HasIndex(utilisateurs => utilisateurs.Matricule).IsUnique();
 }
